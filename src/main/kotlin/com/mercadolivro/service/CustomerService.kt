a4200626b7c3ee89586b27d1d62349233ca68060
Darielly Santos <daryh_cristina@hotmail.com>
2022-08-13 19:29:21 -0300

    feat: created service

---
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -0,0 +1,46 @@
+package com.mercadolivro.service
+
+import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+
+@Service
+class CustomerService {
+
+    val customers = mutableListOf<CustomerModel>()
+
+
+    fun getAll(name: String?): List<CustomerModel> {
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
+        return customers
+    }
+
+    fun create(customer: CustomerModel) {
+        val id = if (customers.isEmpty()) {
+            1
+        } else {
+            (customers.last().id!!.toInt() + 1)
+        }.toString()
+        customer.id = id
+        customers.add(customer)
+    }
+
+    fun getCustomer(id: String): CustomerModel {
+        return customers.first { it.id == id }
+    }
+
+    fun update(customer: CustomerModel) {
+        customers.first { it.id == customer.id }.let {
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    fun delete(id: String) {
+        customers.removeIf() { it.id == id }
+    }
+}
